5879c22a778446b1a6dab6e7e1cb85cd84917c32
aitoehigie <aitoehigie@gmail.com>
2018-03-18 06:13:44 +0100

    add a license file

---
+++ nt-scraper/__init__.py
@@ -0,0 +1,7 @@
+from .__version__ import __version__
+
+__title__ = "Nairaland Terminal Scraper"
+__author__ = "Ehigie (pystar) Aito"
+__license__ = "The MIT License (MIT)"
+__copyright__ = "(c) 2018 Ehigie (pystar) Aito"
+
