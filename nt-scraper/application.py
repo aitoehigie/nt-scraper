0d40907c265a7b4fb943eb4ada41521143e26ce0
aitoehigie <aitoehigie@gmail.com>
2018-03-18 06:05:56 +0100

    create an application.py file

---
+++ nt-scraper/application.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+##########################################
+# Author: Ehigie (pystar) Aito
+# Program: Nairaland.com Terminal Scraper
+# Email: aitoehigie@gmail.com
+# Date: 03/18/2018
+##########################################
+
